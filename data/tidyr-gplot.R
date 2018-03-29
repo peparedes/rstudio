256ccaa7d6b08a416a7f0974c6b68b193038105c
Pablo Paredes <peparedes@gmail.com>
2018-03-29 15:23:16 -0700

    density plot

--- data/tidyr-gplot.R
+++ data/tidyr-gplot.R
@@ -22,3 +22,29 @@ post1990 %>%
                        gdp_mean = mean(gdpPercap), 
                        gdp_sd = sd(gdpPercap))
                        
+
+# Life expectancy vs GDP
+(graph_life_gdp <- ggplot(gapminder, aes(y = lifeExp, x = gdpPercap)) + geom_point(alpha = 0.2) + scale_x_log10())
+  
+#LM
+graph_life_gdp + geom_smooth(method = 'loess', se = FALSE) + 
+  geom_smooth(method = 'lm', color = 'yellow')
+
+#This is bad
+ggplot(gapminder, aes(x = year, y = lifeExp, x = gdpPercap)) +
+  geom_point() +
+  geom_line() +
+  ggtitle(gapminder_filename) + ylab('GDP')
+
+#Facets
+graph_life_gdp + facet_wrap(~ year)
+
+#Back to dplyr
+post1995 <- gapminder %>% filter(year >= 1997)
+ggplot(gapminder, aes(y = lifeExp, x = gdpPercap)) +
+  geom_point(alpha = 0.2) +
+  facet_grid(year ~ continent)
+
+#continents with different shapes and plot population by year
+ggplot(gapminder, aes(gdpPercap, color = continent, fill = continent)) +
+  geom_density(alpha = 0.2) + scale_x_log10() + geom_rug(alpha = 0.5)
